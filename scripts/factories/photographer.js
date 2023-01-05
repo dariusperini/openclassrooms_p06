ce70dd5f96f296301009530271a57ed3a71b6aca
Laurent Abadie <laurent@pepette.co>
2023-01-05 11:26:27 +0100

    photographer factory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+        // Déclaration des éléments du DOM
+        const link = document.createElement('a');
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3' );
+        const txt_tagline  = document.createElement( 'p' );
+        const txt_price  = document.createElement( 'div' );
+
+        // Attribution des valeurs
+        link.href = './pages/photographer.html?id=' + id;
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
         h2.textContent = name;
-        article.appendChild(img);
+        h3.textContent = city + ', ' + country ;
+        txt_tagline.textContent = tagline;
+        txt_price.classList.add('price');
+        txt_price.textContent = price + '€/jour';
+
+        // COnstruction de la fioche photographe
+        article.appendChild(link);
+        link.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(txt_tagline);
+        article.appendChild(txt_price);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,18 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // Recuperation du fichier JSON en utilisant "fetch".
+        return  fetch('./data/photographers.json')
+        .then(function(res) {
+              return res.json();
+          })
+          .then(function(value) {
+            return value;
+          })
+          .catch(function(err) {
+            // Une erreur est survenue
+            console.log(err)
+          });
+        
+
     }
 
     async function displayData(photographers) {
