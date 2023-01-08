917c631dc85bc551448d60116537ed538f2eeb8d
Laurent Abadie <laurent@pepette.co>
2023-01-09 00:31:24 +0100

    photo title and video managment + likes

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,24 +1,39 @@
 function mediaFactory(data) {
-    const {id, photographerId, title, image, video, likes, date, price } = data;
+    const { id, photographerId, title, image, video, likes, date, price } = data;
 
     const imageLink = `../assets/images/${photographerId}/${image}`;
     const videoLink = `../assets/images/${photographerId}/${video}`;
 
-    function addElement(element)
-    {
+    function addElement(element) {
+        let mediaType = '';
+        let img;
+
         const article = document.createElement('article');
-        const img = document.createElement( 'img' );
-        const legend = document.createElement( 'div' );
-        const imageTitle  = document.createElement( 'h3' );
+        if (typeof video === 'undefined') {
+            mediaType = 'img';
+            img = document.createElement('img');
+            img.setAttribute("src", imageLink);
+        }
+        else {
+            mediaType = 'video';
+            img = document.createElement('video');
+            img.setAttribute("src", videoLink);
+            img.controls = "controls";
+        }
+        const legend = document.createElement('div');
+        const imageTitle = document.createElement('h3');
+        const likeDisplay = document.createElement('h3');
 
         article.appendChild(img);
-        img.setAttribute("src", imageLink);
         img.setAttribute("alt", title);
         imageTitle.textContent = title;
-        imageTitle.setAttribute("aria-label", title);
-        img.classList.add("book_photo");
+        likeDisplay.textContent = likes;
+        imageTitle.setAttribute("aria-label", "Titre du média");
+        img.classList.add("book_"+mediaType);
         legend.classList.add("legend");
+        likeDisplay.classList.add("likes");
         legend.appendChild(imageTitle);
+        legend.appendChild(likeDisplay);
         article.appendChild(legend);
 
 
